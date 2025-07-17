6616707170db770e53c3d1812bf7aa7797a53591
ZSedillo <sedillozandro720@gmail.com>
2025-07-17 10:56:16 +0800

    debuging server.js

--- server/server.js
+++ server/server.js
@@ -10,11 +10,11 @@ const startServer = async () => {
         const documents = await getAllTempDocuments();
         console.log("📄 Documents fetched:", documents);
 
-        if (!documents.length) {
-            console.log("⚠️ No documents found in the 'temps' collection.");
-        } else {
-            documents.forEach(doc => console.log(doc));
-        }
+        // if (!documents.length) {
+        //     console.log("⚠️ No documents found in the 'temps' collection.");
+        // } else {
+        //     documents.forEach(doc => console.log(doc));
+        // }
 
     } catch (err) {
         console.error("❌ Error:", err.message);
