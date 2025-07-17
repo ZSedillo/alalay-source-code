e8c6efe3bd2044392db94019a65bc2ad1da3bc9f
ZSedillo <sedillozandro720@gmail.com>
2025-07-17 09:40:03 +0800

    testing tempController

--- server/controllers/tempController.js
+++ server/controllers/tempController.js
@@ -1,9 +1,33 @@
+// const { getAllTempDocuments } = require('../models/TempModel.js');
+
+// async function fetchAndLogTempDocs() {
+//     try {
+//         const documents = await getAllTempDocuments();
+//         documents.forEach(doc => console.log(doc));
+//     } catch (err) {
+//         console.error("Error in Controller:", err);
+//     }
+// }
+
+// module.exports = { fetchAndLogTempDocs };
+
 const { getAllTempDocuments } = require('../models/TempModel.js');
 
 async function fetchAndLogTempDocs() {
     try {
         const documents = await getAllTempDocuments();
-        documents.forEach(doc => console.log(doc));
+
+        documents.forEach(doc => {
+            const id = doc._id.toString();
+            const name = doc.username || doc.name || 'No Name';
+            const age = doc.age || 'Unknown';
+
+            console.log(`ID: ${id}`);
+            console.log(`Name: ${name}`);
+            console.log(`Age: ${age}`);
+            console.log('-----------------------------');
+        });
+
     } catch (err) {
         console.error("Error in Controller:", err);
     }
