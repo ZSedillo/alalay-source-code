e1d5d27ac4a9eebe0ead5aefc52e8a29f6f91cff
ZSedillo <sedillozandro720@gmail.com>
2025-07-17 09:52:02 +0800

    adding codes in db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,14 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config({ path: __dirname + '/config.env' });
 
 const uri = process.env.ATLAS_URI;
+
+if (!uri) {
+    console.error("❌ MongoDB URI is missing! Check your config.env path and variable.");
+    process.exit(1);
+}
+
+console.log("✅ MongoDB URI loaded!");
+
 const client = new MongoClient(uri);
 
 async function connectDB() {
